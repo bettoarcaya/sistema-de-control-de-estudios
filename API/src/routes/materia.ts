2a883fa690f54d5cc53e2d6edae581a430655358
Humberto Arcaya <betoarcaya@gmail.com>
2019-05-06 18:38:07 -0400

    agregando ruta para obtener una materia en especifico mediante su id

--- API/src/routes/materia.ts
+++ API/src/routes/materia.ts
@@ -1,11 +1,21 @@
 import { Router } from "express";
 import { checkJwt } from "../middlewares/checkJwt";
 import { checkRole } from "../middlewares/checkRole";
-import { MateriaController } from "../controllers/MateriaController";
+import MateriaController from "../controllers/MateriaController";
 
 const router = Router();
 
 
-router.get("/", [checkJwt, checkRole(["ADMIN"])], MateriaController.listAll)
+router.get(
+    "/", 
+    [checkJwt, checkRole(["ADMIN"])], 
+    MateriaController.listAll
+);
+
+router.get(
+    "/:id([0-9]+)",
+    [checkJwt, checkRole(["ADMIN"])],
+    MateriaController.getOneById
+);
 
 export default router;
