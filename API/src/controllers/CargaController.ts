ac4b04a2e0ca40a1a1aaf6de6a568869ab85518c
Humberto Arcaya <betoarcaya@gmail.com>
2019-06-01 21:10:14 -0400

    metodo para obtener la carga academica de un usuario

---
+++ API/src/controllers/CargaController.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from "express";
+import { Materia } from "../entity/Materia";
+import { getRepository } from "typeorm";
+import { User } from "../entity/User";
+import { Validate, validate } from "class-validator";
+import { Carga } from "../entity/Carga";
+
+class CargaController {
+
+    static listarCargaPorUsuario = async (req: Request, res: Response) => {
+        const id = req.params.id;
+        const userRepository = getRepository(User); 
+        const cargaRepository = getRepository(Carga);
+        let carga;
+
+        try{
+            //carga = await cargaRepository.findOneOrFail(id); 
+            carga = await cargaRepository.find({
+                where: { userId: id }
+            }); 
+         }catch (error){
+             res.status(404).send("Este usuario no tiene carga aun");
+         }
+
+        res.send(carga);
+    }
+
+
+
+};
+
+export default CargaController; 
