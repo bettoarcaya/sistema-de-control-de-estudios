a2667cdd8ca854afe2ee1c0389c8e55c82b1f257
Humberto Arcaya <betoarcaya@gmail.com>
2019-06-01 21:32:20 -0400

    WIP: trabajando en el metodo para inscribir las materias de los estudiantes

--- API/src/controllers/CargaController.ts
+++ API/src/controllers/CargaController.ts
@@ -8,20 +8,25 @@ import { Carga } from "../entity/Carga";
 class CargaController {
 
     static listarCargaPorUsuario = async (req: Request, res: Response) => {
-        const id = req.params.id;
+        const usuarioId = req.params.estudianteId;
         const cargaRepository = getRepository(Carga);
         let carga;
 
         try{
             //carga = await cargaRepository.findOneOrFail(id); 
             carga = await cargaRepository.findOneOrFail({
-                where: { userId: id }
+                where: { userId: usuarioId }
             }); 
          }catch (error){
              res.status(404).send("Este usuario no tiene carga aun");
          }
 
         res.send(carga);
+    }
+
+    static inscribirMaterias = async (req: Request, res: Response) => {
+        const usuarioId = req.params.estudianteId;
+        const { Carga } = req.body;
     }
 
 
