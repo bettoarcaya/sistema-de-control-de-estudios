e192f3f7aa17b40a647bc2bb9dc25e12d9dc7307
Humberto Arcaya <betoarcaya@gmail.com>
2019-06-04 19:52:11 -0400

    eliminado las rutas inexistentes

--- API/src/routes/index.ts
+++ API/src/routes/index.ts
@@ -1,14 +1,10 @@
 import { Router, Request, Response } from "express";
 import auth from "./auth";
 import user from "./user";
-import materia from "./materia";
-import carga from "./carga";
 
 const routes = Router();
 
 routes.use("/auth", auth);
 routes.use("/user", user);
-routes.use("/materia", materia);
-routes.use("/carga", carga);
 
 export default routes;
